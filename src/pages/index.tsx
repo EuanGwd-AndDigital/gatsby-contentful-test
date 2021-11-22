25ef0ee038d65a3b339056e6c4055d40c77aca87
Euan Greenwood <euan.greenwood@and.digital>
2021-11-22 10:38:14 +0000

    add apollo queries

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,47 @@
 import React from 'react'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from '@apollo/client'
 import styled from 'styled-components'
 
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/___graphql',
+  cache: new InMemoryCache(),
+})
+
 const IndexPage = () => {
+  const { data } = useQuery(HOME_PAGE_QUERY)
+  // const { nodes } = data?.allContentfulBlogPost
+
   return (
-    <Main>
-      <title> Gatsby Blog</title>
-      <Header>Home Page</Header>
-    </Main>
+    <ApolloProvider client={client}>
+      <Main>
+        <title>Gatsby Blog</title>
+        <Header>Home Page</Header>
+
+        <ul>
+          {data?.allContentfulBlogPost.nodes.map((node) => (
+            <li>{node?.title}</li>
+          ))}
+        </ul>
+      </Main>
+    </ApolloProvider>
   )
 }
+
+const HOME_PAGE_QUERY = gql`
+  query BlogListing {
+    allContentfulBlogPost {
+      nodes {
+        title
+      }
+    }
+  }
+`
 
 const Main = styled.main`
   display: grid;
